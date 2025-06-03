8b01e2df8af29155bc7159140834f7d8eb9d9851
QunnTr <nauq261@gmail.com>
2025-06-03 14:52:56 +0700

    .

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ const PORT = process.env.PORT || 5000;
 // CORS configuration
 const allowedOrigins = [
   "http://localhost:5173",
-  "hmern-fashion-shop-client.vercel.app", // Thêm domain của Vercel
-  "https://mern-fashion-shop-client-git-main-quannt.vercel.app", // Thêm domain preview của Vercel
+  "https://mern-fashion-shop-client.vercel.app",
+  "https://mern-fashion-shop-client-git-main-quannt.vercel.app"
 ];
 
 app.use(
@@ -41,10 +41,9 @@ app.use(
     origin: function (origin, callback) {
       // Allow requests with no origin (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-
+      
       if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
+        const msg = "The CORS policy for this site does not allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
       return callback(null, true);
