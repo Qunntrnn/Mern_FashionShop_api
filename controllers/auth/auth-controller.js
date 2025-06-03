3a8baf45cfd4ea56a219b4dfb49e678dd5be82cc
QunnTr <nauq261@gmail.com>
2025-06-03 14:28:48 +0700

    .

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../../models/user");
+const User = require("../../models/User");
 const { authMiddleware } = require("../../middleware/auth");
 
 const TOKEN_EXPIRY = "24h"; // Increase token expiry to 24 hours
